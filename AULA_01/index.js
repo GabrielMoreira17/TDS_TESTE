8dbbad971a25b5e22a3d175e8cc7a4f9209918c9
gmoreira2128 <gabrielmoreira2128@gmail.com>
2025-02-19 17:05:05 -0300

    19/02/2025

--- AULA_01/index.js
+++ AULA_01/index.js
@@ -1,11 +1,22 @@
+// Importando o módulo pessoa
 const pessoa  = require("./pessoa");
-const DeletaPessoa = require("./pessoa");
 
+// Cadastra pessoas
 pessoa.cadastraPessoa("Gabriel", 17);
 pessoa.cadastraPessoa("Lucas", 18);
 pessoa.cadastraPessoa("Pedro", 19);
+pessoa.cadastraPessoa("João", 20);
 
+// Lista pessoas
 console.log(pessoa.listaPessoas());
 
+// Deleta a pessoa com id 1
 pessoa.DeletaPessoa(1);
+
+
+// Atualiza a pessoa com id passado
+pessoa.AtualizaPessoas(2, "Gustavo", 30);
+
+
+// Lista pessoas depois de deletar
 console.log(pessoa.listaPessoas());
--- AULA_01/pessoa.js
+++ AULA_01/pessoa.js
@@ -8,8 +8,9 @@
 
 // ID, NOME, IDADE
 const pessoas = [];
-var idSeq = 0;
+var idSeq = 1;
 
+//Cadastra uma pessoa com nome e idade
 function cadastraPessoa(nome, idade)
 {
     var pessoa = 
@@ -23,27 +24,53 @@ function cadastraPessoa(nome, idade)
     return pessoa;
 }
 
+
 //função que apresenta a lista de pessoas
 function listaPessoas()
 {
     return pessoas;
 }
 
+
+//função que deleta uma pessoa atraves do id
 function DeletaPessoa(id)
 {
-    var index = pessoas.findIndex(pessoa => pessoa.id == id);
-    if(index != -1)
+    //Encontra o index da pessoa com o id passado
+    for(let i = 0; i < pessoas.length; i++)
     {
-        pessoas.splice(index, 1);
-        return true;
+        if(pessoas[i].id == id)
+        {
+            pessoas.splice(i, 1);
+            console.log("Pessoa deletada com sucesso!");
+            return true;
+        }
     }
     return false;
 }
 
-module.exports = {cadastraPessoa, listaPessoas, DeletaPessoa};
 
-// console.log(cadastraPessoa("Gabriel", 17));
-// console.log(cadastraPessoa("Lucas", 18));
-// console.log(cadastraPessoa("Pedro", 19));
+function AtualizaPessoas(id, nome, idade)
+{
+    for(let i = 0; i < pessoas.length; i++)
+    {
+        if(pessoas[i].id == id)
+        {
+            pessoas[i].nome = nome;
+            pessoas[i].idade = idade;
+            console.log("Pessoa atualizada com sucesso!");
+            return true;
+        }
+        else{
+            console.log("Pessoa não encontrada!");
+            return false;
+        }
+    }
+}
 
-// console.log(listaPessoas());
+//Exporta as funções para serem utilizadas em outros arquivos
+module.exports = {
+    cadastraPessoa,
+    listaPessoas, 
+    DeletaPessoa,
+    AtualizaPessoas
+};
